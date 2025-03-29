d9549ef2b9a722990f1ad1a9ee1499badbac1234
Adam Seaton <113870480+Aps-x@users.noreply.github.com>
2025-03-29 21:29:57 +1100

    CSS is almost done

--- js/random_sort.js
+++ js/random_sort.js
@@ -3,6 +3,6 @@ const list_of_cards = document.querySelectorAll(".card");
 // Randomly position, size, and color the cards
 list_of_cards.forEach(element => {
     element.style.gridRowStart = Math.floor(Math.random() * 6) + 2; 
-    element.style.gridRowEnd = `span ${Math.floor(Math.random() * 3) + 1}`; 
+    element.style.gridRowEnd = `span ${Math.floor(Math.random() * 3) + 2}`; 
     element.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16).padStart(6, '0')}`;
 });
